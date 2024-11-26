e11a3a04c4e2cb603a780ae592071943e268a6e9
urambatyam <salt90502@gmail.com>
2024-11-26 23:54:11 +0100

    menu

--- Athena/src/app/menu/menu.component.ts
+++ Athena/src/app/menu/menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [ RouterLink, RouterLinkActive],
+  imports: [ RouterLink, RouterLinkActive, MatButtonModule, MatMenuModule, MatToolbarModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
